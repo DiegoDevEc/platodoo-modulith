49680b163a7a3efc9acb1456686c647e17fe2d94
Diego Acosta <diego.acosta@externos.bci.cl>
2025-05-26 18:08:53 -0500

    Test con h2

--- src/test/java/com/playtodoo/modulith/SportComplexServiceImplTest.java
+++ src/test/java/com/playtodoo/modulith/SportComplexServiceImplTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 class SportComplexServiceImplTest {
 
     @Mock
